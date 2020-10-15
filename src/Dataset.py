7cc79256925a4ec3f9ab0e507f305ff5b5f4e21c
Abram Schonfeldt <schabr004@myuct.ac.za>
2020-10-15 21:29:38 +0200

    Updates dataset

--- src/Dataset.py
+++ src/Dataset.py
@@ -5,46 +5,47 @@ import random as rnd
 import tensorflow as tf
 
 def main():
-    train_ds, val_ds, test_ds = load_data(0.1) #load 10% if the data
+  train_ds, val_ds, test_ds = load_data(0.1) #load 10% of the data
 
-    #check out the first few images
-    for img, lab in train_ds.unbatch().take(1):
-        visualize(img)
-        print(f"Label: {lab}")
-    
+  #check out the first image
+  for img, lab in train_ds.unbatch().take(1):
+      visualize(img)
+      print(f"Label: {lab}")
+
+
 
 def load_data(prop=1):
-    #get file paths for the rfis and frbs
-    rfi_names = tf.io.gfile.glob("data/label_0/*.jpg") #assumes testing from 'root' directiory
-    frb_names = tf.io.gfile.glob("data/label_1/*.jpg")
-    all_names = rfi_names + frb_names
-    rnd.seed(17) #set seed important for reproducibility 
-    rnd.shuffle(all_names) #shuffle names
+  #get file paths for the rfis and frbs
+  rfi_names = tf.io.gfile.glob("data/label_0/*.jpg") #assumes testing from 'root' directiory
+  frb_names = tf.io.gfile.glob("data/label_1/*.jpg")
+  all_names = rfi_names + frb_names
+  rnd.seed(17) #set seed important for reproducibility 
+  rnd.shuffle(all_names) #shuffle names
 
-    #define train, validation, test counts
-    no_total = round(len(all_names)*prop) 
-    no_train = round(0.7 * no_total)
-    no_val = round(0.15 * no_total)
-    no_test = no_total - no_train - no_val          
+  #define train, validation, test counts
+  no_total = round(len(all_names)*prop) 
+  no_train = round(0.7 * no_total)
+  no_val = round(0.15 * no_total)
+  no_test = no_total - no_train - no_val          
 
-    # create datasets of the file names
-    list_ds = tf.data.Dataset.from_tensor_slices(all_names)
-    train_list_ds = list_ds.take(no_train)
-    val_list_ds = list_ds.skip(no_train).take(no_val)
-    test_list_ds = list_ds.skip(no_train + no_val).take(no_test)
+  # create datasets of the file names
+  list_ds = tf.data.Dataset.from_tensor_slices(all_names)
+  train_list_ds = list_ds.take(no_train)
+  val_list_ds = list_ds.skip(no_train).take(no_val)
+  test_list_ds = list_ds.skip(no_train + no_val).take(no_test)
 
-    # create actual datasets
-    AUTOTUNE = tf.data.experimental.AUTOTUNE #read into this more
-    train_ds = train_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-    val_ds = val_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-    test_ds = test_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+  # create actual datasets
+  AUTOTUNE = tf.data.experimental.AUTOTUNE #read into this more
+  train_ds = train_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+  val_ds = val_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+  test_ds = test_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
 
-    #configure to be batched, etc.
-    train_ds = configure_for_performance(train_ds, train=True)
-    val_ds = configure_for_performance(val_ds)
-    test_ds = configure_for_performance(test_ds)
+  #configure to be batched, etc.
+  train_ds = configure_for_performance(train_ds, train=True)
+  val_ds = configure_for_performance(val_ds)
+  test_ds = configure_for_performance(test_ds)
 
-    return train_ds, val_ds, test_ds
+  return train_ds, val_ds, test_ds
 
 
 def configure_for_performance(ds, batch_size=32, train=False): #cache, batch and prefetch data
@@ -58,30 +59,30 @@ def configure_for_performance(ds, batch_size=32, train=False): #cache, batch and
 
 #======================================functions for processing images
 def illustration(): #illustrates the process
-    rfi_names = tf.io.gfile.glob("data/label_0/*.jpg") #assumes testing from 'root' directiory
-    frb_names = tf.io.gfile.glob("data/label_1/*.jpg")
-    print(len(rfi_names), len(frb_names)) # expect (10371, 10944)
+  rfi_names = tf.io.gfile.glob("data/label_0/*.jpg") #assumes testing from 'root' directiory
+  frb_names = tf.io.gfile.glob("data/label_1/*.jpg")
+  print(len(rfi_names), len(frb_names)) # expect (10371, 10944)
 
-    ex_path = tf.io.read_file(frb_names[10000])
-    img = tf.image.decode_jpeg(ex_path, channels=3) # convert the compressed string to a 3D uint8 tensor
-    print(f"Dimensions {img.shape}")
-    print(img[1,1,:].numpy()) #inspect the first pixel
+  ex_path = tf.io.read_file(frb_names[10000])
+  img = tf.image.decode_jpeg(ex_path, channels=3) # convert the compressed string to a 3D uint8 tensor
+  print(f"Dimensions {img.shape}")
+  print(img[1,1,:].numpy()) #inspect the first pixel
 
-    #convert the uint8 tensor  to float32
-    img = tf.image.convert_image_dtype(img, tf.float32) #this also has the effect of squishing all pixel values to [0,1]
-    print(img[1,1,:].numpy()) #inspect the first pixel
-    
-    #convert to grayscale
-    img = tf.image.rgb_to_grayscale(img)  
-    print(img[1,1,:].numpy()) #inspect the first pixel - notice the collapse along the frequency channels
+  #convert the uint8 tensor  to float32
+  img = tf.image.convert_image_dtype(img, tf.float32) #this also has the effect of squishing all pixel values to [0,1]
+  print(img[1,1,:].numpy()) #inspect the first pixel
+  
+  #convert to grayscale
+  img = tf.image.rgb_to_grayscale(img)  
+  print(img[1,1,:].numpy()) #inspect the first pixel - notice the collapse along the frequency channels
 
-    #crop based on size
-    if img.shape[0] == 689:                 
-        img = crop_1(img)
-    else:
-        img = crop_2(img)
-    
-    visualize(img)
+  #crop based on size
+  if img.shape[0] == 689:                 
+      img = crop_1(img)
+  else:
+      img = crop_2(img)
+  
+  visualize(img)
 
 
 def get_label(file_path):               #True for frb, False for rfi
@@ -110,7 +111,6 @@ def process_path(file_path):
   img = tf.io.read_file(file_path)      #load the raw data from the file as a string
   img = decode_img(img)
   return img, label
-
 
 def visualize(image):                   #convenient function for plotting
   plt.imshow(tf.squeeze(image), vmin=0, vmax=1, cmap='Greys')
